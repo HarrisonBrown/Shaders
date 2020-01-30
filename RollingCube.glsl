1033da7bb36051006806e530190aa3aa00cdfd14
Harrison Brown <harrison.brown@live.co.uk>
2020-01-30 22:34:43 +0000

    rolling cube colour alterations - make shadow red

--- RollingCube.glsl
+++ RollingCube.glsl
@@ -1,17 +1,12 @@
-// "ShaderToy Tutorial - Ray Marching Operators 2"
-// by Martijn Steinrucken aka BigWings/CountFrolic - 2019
-// License Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported
-// License.
-//
-// This is the starting point for a YouTube tutorial:
-// https://youtu.be/Vmb7VGBVZJA
-
 #define MAX_STEPS 10000
 #define MAX_DIST 100.
 #define SURF_DIST .001
 
 #define PI 3.1415926535897932384626433832795
-#define rollspeed iTime
+#define ROLLSPEED iTime
+
+#define CUBE_COLOUR vec3(.5, .933, .969)
+#define PLANE_COLOUR vec3(.8, .843, .922)
 
 mat2 Rot(float a) {
   float s = sin(a);
@@ -42,20 +37,21 @@ float sdBox(vec3 p, vec3 s) {
   return length(max(p, 0.)) + min(max(p.x, max(p.y, p.z)), 0.);
 }
 
-vec4 GetDist(vec3 p) {
-  vec3 planeColour = vec3(.5, .5, .5);
-  vec3 boxColour = vec3(1, 0, 1);
-
-  vec4 plane = vec4(planeColour, p.y);
-
-  //Cube
+vec4 GetCubeDist(vec3 p)
+{
   float sqrt2 = 1.41421356237;
-  float yMov = max(sin(2.*rollspeed), sin(2.*rollspeed + PI))/(sqrt2+1.) + 1.;
-  vec3 rotatedCubePos = rotation(p-vec3(0,yMov,0), vec3(0,0,1), rollspeed);
+  float yMov = max(sin(2.*ROLLSPEED), sin(2.*ROLLSPEED + PI))/(sqrt2+1.) + 1.;
+  vec3 rotatedCubePos = rotation(p-vec3(0,yMov,0), vec3(0,0,1), ROLLSPEED);
   rotatedCubePos-=vec3(0,-1,0);
 
+  return vec4(CUBE_COLOUR, sdBox(rotatedCubePos - vec3(0, 1, 0), vec3(1)));
+}
+vec4 GetDist(vec3 p) {
+  //Floor
+  vec4 plane = vec4(PLANE_COLOUR, p.y);
 
-  vec4 box = vec4(boxColour, sdBox(rotatedCubePos - vec3(0, 1, 0), vec3(1)));
+  //Cube
+  vec4 box = GetCubeDist(p);
 
   return plane.w < box.w ? plane : box;
 }
@@ -81,24 +77,47 @@ vec3 GetNormal(vec3 p) {
   float d = GetDist(p).w;
   vec2 e = vec2(.001, 0);
 
-  vec3 n = d - vec3(GetDist(p - e.xyy).w, GetDist(p - e.yxy).w, GetDist(p - e.yyx).w);
+  vec3 n = d - vec3(GetCubeDist(p - e.xyy).w, GetCubeDist(p - e.yxy).w, GetCubeDist(p - e.yyx).w);
 
   return normalize(n);
 }
 
-float GetLight(vec3 p) {
-  vec3 lightPos = vec3(3, 5, 4);
+float Shadow(vec3 p) {
+  vec3 lightPos = vec3(0, 5, -25);
   vec3 l = normalize(lightPos - p);
-  vec3 n = GetNormal(p);
+  
+  //Get Normal
+  float d = GetDist(p).w;
+  vec2 e = vec2(.001, 0);
 
-  float dif = clamp(dot(n, l) * .5 + .5, 0., 1.);
+  vec3 n = d - vec3(GetDist(p - e.xyy).w, GetDist(p - e.yxy).w, GetDist(p - e.yyx).w);
+  n = normalize(n);
+
+  float dif = 1.;
 
   // Shadow
-  float d = RayMarch(p + n * SURF_DIST * 2., l).w;
-  if(p.y<.01 && d<length(lightPos-p)) dif *= .5;
+  float d2 = RayMarch(p + n * SURF_DIST * 2., l).w;
+  if(p.y<.01 && d2<length(lightPos-p)) dif *= .5;
 
   return dif;
 }
+
+float DiffuseLight(vec3 p){  
+  vec3 lightPos = vec3(0, 5, -25);
+  vec3 l = normalize(lightPos - p);
+
+  //Get Normal
+  float d = GetDist(p).w;
+  vec2 e = vec2(.001, 0);
+
+  vec3 n = d - vec3(GetCubeDist(p - e.xyy).w, GetCubeDist(p - e.yxy).w, GetCubeDist(p - e.yyx).w);
+  n = normalize(n);
+
+  float dif = clamp(dot(n, l) * .5 + .5, 0., 1.);
+
+  return dif;
+}
+
 
 vec3 R(vec2 uv, vec3 p, vec3 l, float z) {
   vec3 f = normalize(l - p), r = normalize(cross(vec3(0, 1, 0), f)),
@@ -122,10 +141,12 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
   vec4 d = RayMarch(ro, rd);
 
   if (d.w < MAX_DIST) {
-    vec3 p = ro + rd * d.w;
+    col = d.xyz;
 
-    float dif = GetLight(p);
-    col = d.xyz * dif;
+    vec3 p = ro + rd * d.w;
+    float dif = DiffuseLight(p);
+    col.xyz *= dif;
+    col.yz *= Shadow(p);
   }
 
   fragColor = vec4(col, 0.0);
